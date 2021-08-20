d8037950f107a9d666c11658e86bad33da1a948e
Danielpv02 <88729020+Danielpv02@users.noreply.github.com>
2021-08-19 20:04:47 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,10 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-print("Este programa calcula el área de un triángulo")
-h = float(input("Dame la altura: "))
-b = float(input("Dame la base: "))
-print("El área del triángulo es: " + str((b*h)/2) )
+    b = float(input("Dame la base: "))
+    h = float(input("Dame la altura: "))
+    print("El área es: " + str((b*h)/2) )
 
 if __name__ == '__main__':
     main()
